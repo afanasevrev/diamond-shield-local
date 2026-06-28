e5c86d211fadf3e1615ce9dc4761e9bd8f113b3e
revoly afanasev <rewoly@mail.ru>
2026-06-28 15:20:14 +0900

    Initial commit

---
+++ src/main/java/ru/diamondshield/local/DiamondShieldLocalApplication.java
@@ -0,0 +1,13 @@
+package ru.diamondshield.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondShieldLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondShieldLocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/diamondshield/local/DiamondShieldLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diamondshield.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondShieldLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

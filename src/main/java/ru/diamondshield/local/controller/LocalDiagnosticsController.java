453eecdcd323fd4272a1c6429692b944006ff168
revoly afanasev <rewoly@mail.ru>
2026-07-01 01:08:43 +0900

    Обновил конфиг по Perco

--- src/main/java/ru/diamondshield/local/controller/LocalDiagnosticsController.java
+++ src/main/java/ru/diamondshield/local/controller/LocalDiagnosticsController.java
@@ -72,7 +72,7 @@ public class LocalDiagnosticsController {
         result.put("controllersNote", "Controllers are received from central server config and stored in local DB");
 
         result.put("percoEnabled", properties.getPerco().isEnabled());
-     
+
 
         LocalConfigState state = configStateRepository
                 .findFirstByLocalServerId(properties.getLocalServer().getId())
